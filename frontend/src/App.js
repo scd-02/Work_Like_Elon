ad3238b2134fdcab2403d9f07cdf80294dedbafc
Anushka Antra <antraanushka31@gmail.com>
2023-10-12 04:21:17 +0530

    added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import LoginPage from './utilities/LoginPage/LoginPage';
+import LoginPage from './utilities/LoginPage/LoginPage';
 // import TODO from './utilities/HomePage/TODO';
-import TaskList from './utilities/TaskBar/TaskList';
+// import TaskList from './utilities/TaskBar/TaskList';
 
 function App() {
   return (
     // <TODO />
-    <TaskList />
-    // <LoginPage />
+    // <TaskList />
+    <LoginPage />
   );
 }
 
